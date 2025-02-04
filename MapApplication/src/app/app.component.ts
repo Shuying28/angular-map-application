a92487ffabfbb9378d4a6a9c56ffaba9950b2142
Ong Shu Ying <ongshuyiing@gmail.com>
2025-02-04 23:55:10 +0800

    add main point form & additional points form

--- MapApplication/src/app/app.component.ts
+++ MapApplication/src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { MainPointComponent } from './main-point/main-point.component';
+import { AdditionalPointsComponent } from './additional-points/additional-points.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'MapApplication';
+  constructor(private modalService: NzModalService) {}
+
+  showMainPointModal() {
+    const modal = this.modalService.create({
+      nzTitle: 'Manage Main Point',
+      nzContent: MainPointComponent,
+      nzFooter: null,
+    });
+  }
+
+  showAdditionalPointsModal() {
+    const modal = this.modalService.create({
+      nzTitle: 'Add Additional Points',
+      nzContent: AdditionalPointsComponent,
+      nzFooter: null,
+    });
+  }
 }
--- MapApplication/src/app/app.module.ts
+++ MapApplication/src/app/app.module.ts
@@ -12,25 +12,31 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MainPointComponent } from './main-point/main-point.component';
 import { AdditionalPointsComponent } from './additional-points/additional-points.component';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { ReactiveFormsModule } from '@angular/forms';
 
 registerLocaleData(en);
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainPointComponent,
-    AdditionalPointsComponent
-  ],
+  declarations: [AppComponent, MainPointComponent, AdditionalPointsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NzFormModule,
+    NzInputModule,
+    NzButtonModule,
+    NzLayoutModule,
+    ReactiveFormsModule,
+    NzModalModule,
   ],
-  providers: [
-    { provide: NZ_I18N, useValue: en_US }
-  ],
-  bootstrap: [AppComponent]
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
